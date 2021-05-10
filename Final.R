9aa8482451047d37aae8e1542c43cf8cb6474ba1
DuLiCS <ieee811@gmail.com>
2021-05-10 11:08:02 +0800

    Update Final.R

--- Final.R
+++ Final.R
@@ -7,11 +7,30 @@ setwd("Programme/Spare-Work/20210508free/code/")
 covid_data <- read.csv("covid19-download.csv")
 canda_data <- subset(covid_data, pruid == 1) 
 Edmonton_climate <- read.csv("Edmonton_climate_daily_Alberta_P1D.csv")
+Edmonton_climate_2020 <- read.csv("en_climate_daily_AB_3012209_2020_P1D.csv")
+Edmonton_climate <- rbind(Edmonton_climate_2020,Edmonton_climate)
+
 Halifax_climate <- read.csv("Halifax_climate_daily_Nova Scotia_P1D.csv")
+Halifax_climate_2020 <- read.csv("en_climate_daily_NS_8202251_2020_P1D.csv")
+Halifax_climate <- rbind(Halifax_climate_2020, Halifax_climate)
+
 Montreal_climate <- read.csv("Montreal_climate_daily_Quebec_P1D.csv")
+Montreal_climate_2020 <- read.csv("en_climate_daily_QC_7025251_2020_P1D.csv")
+Montreal_climate <- rbind(Montreal_climate_2020, Montreal_climate)
+
 StJohn_climate <- read.csv("St.John's_climate_daily_Newfounland and Labrador_P1D.csv")
+StJohn_climate_2020 <- read.csv("en_climate_daily_NL_8403505_2020_P1D.csv")
+StJohn_climate <- rbind(StJohn_climate_2020, StJohn_climate)
+
 Toronto_climate <- read.csv("Toronto_climate_daily_Ontario_P1D.csv")
+Toronto_climate_2020 <- read.csv("en_climate_daily_ON_6158355_2020_P1D.csv")
+Toronto_climate<- rbind(Toronto_climate_2020, Toronto_climate)
+
+
 Vancouver_climate <- read.csv("Vancouver_climate_daily_British Columbia_P1D.csv")
+Vancouver_climate_2020 <- read.csv("en_climate_daily_BC_1108395_2020_P1D.csv")
+Vancouver_climate <- rbind(Vancouver_climate_2020,Vancouver_climate)
+
 
 Edmonton_data <- subset(covid_data, pruid == 48)
 colnames(Edmonton_climate)[5] = 'date'
@@ -20,8 +39,9 @@ mean_temp <- c()
 num_confirmed <- c()
 total_precip <- c()
 
-for (m in 1:5)
-{temp_set <- subset(Edmonton_merge, Month == m) 
+for (y in 2020:2021)
+{for (m in 1:12)
+{temp_set <- subset(Edmonton_merge, Month == m & Year == y) 
 print(m)
 early_month <- subset(temp_set, Day>=1 & Day<15)
 early_month <- subset(early_month, !is.na(Mean.Temp...C.))
@@ -33,15 +53,15 @@ num_confirmed[(length(num_confirmed) + 1)] <- sum(early_month[,16])
 num_confirmed[(length(num_confirmed) + 1)] <- sum(late_month[,16])
 total_precip[(length(total_precip) + 1)] <- sum(early_month[,64])
 total_precip[(length(total_precip) + 1)] <- sum(late_month[,64])
-}
+}}
 #date_stamp <- c("Early Jan","Late Jan","Early Feb","Late Feb","Early Mar","Late Mar","Early April","Late April","Early May","Late May")
-date_stamp <- c(1,2,3,4,5,6,7,8,9,10)
+date_stamp <- c(1:48)
 dd <- data.frame(date_stamp,mean_temp,num_confirmed,total_precip)
 dd <- subset(dd, !is.na(mean_temp))
 summary(dd)
 p <- ggplot(dd, aes(x = date_stamp))+
-  geom_line(aes(y = mean_temp, color = "mean temperature"))+
-  geom_line(aes(y = (num_confirmed - 19000)/600, color = "num of confirmed cases"))+
+  geom_point(aes(y = mean_temp, color = "mean temperature"))+
+  geom_point(aes(y = (num_confirmed - 19000)/600, color = "num of confirmed cases"))+
   scale_y_continuous(sec.axis = sec_axis(~.*600 + 19000, name = "num of confirmed cases"))+
   scale_color_manual(values = c("red","green"))+
   labs(title = "Edmonton")+
@@ -49,19 +69,29 @@ p <- ggplot(dd, aes(x = date_stamp))+
         axis.title.y = element_text(size = 12))
 ggsave(p,filename = "pEdmonton.png",width = 10,height = 10,limitsize = F)
 
+p <- ggplot(Edmonton_merge, aes(x = date))+
+  geom_point(aes(y = Mean.Temp...C., color = "mean temperature"))+
+  geom_point(aes(y = (numconf - 19000)/600, color = "num of confirmed cases"))+
+  scale_y_continuous(sec.axis = sec_axis(~.*600 + 19000, name = "num of confirmed cases"))+
+  scale_color_manual(values = c("red","green"))+
+  labs(title = "Edmonton")+
+  theme(plot.title = element_text(hjust = 0.5),
+        axis.title.y = element_text(size = 12))
+ggsave(p,filename = "pEdmontonAll.png",width = 10,height = 10,limitsize = F)
+
+
+
 a<-2000
 b<-4000
 p <- ggplot(dd, aes(x = date_stamp))+
-  geom_line(aes(y = total_precip, color = "total precip"))+
-  geom_line(aes(y = (num_confirmed - b)/a, color = "num of confirmed cases"))+
+  geom_point(aes(y = total_precip, color = "total precip"))+
+  geom_point(aes(y = (num_confirmed - b)/a, color = "num of confirmed cases"))+
   scale_y_continuous(sec.axis = sec_axis(~.*a + b, name = "num of confirmed cases"))+
   scale_color_manual(values = c("red","green"))+
   labs(title = "Edmonton")+
   theme(plot.title = element_text(hjust = 0.5),
         axis.title.y = element_text(size = 12))
 ggsave(p,filename = "precipEdmonton.png",width = 10,height = 10,limitsize = F)
-
-
 
 
 #Edmonton 48   Halifax 12 Montreal 24 StJohn's 10 Toronto 35 Vancouver 59
@@ -72,8 +102,9 @@ mean_temp <- c()
 num_confirmed <-c()
 total_precip <- c()
 
-for (m in 1:5)
-{temp_set <- subset(Halifax_merge, Month == m) 
+for (y in 2020:2021)
+{for (m in 1:12)
+{temp_set <- subset(Halifax_merge, Month == m & Year == y) 
 print(m)
 early_month <- subset(temp_set, Day>=1 & Day<15)
 early_month <- subset(early_month, !is.na(Mean.Temp...C.))
@@ -89,17 +120,17 @@ late_month <- subset(temp_set, Day>=15 & Day <=31)
 late_month <- subset(late_month, !is.na(Total.Precip..mm.))
 total_precip[(length(total_precip) + 1)] <- sum(early_month[,64])
 total_precip[(length(total_precip) + 1)] <- sum(late_month[,64])
-}
+}}
 #date_stamp <- c("Early Jan","Late Jan","Early Feb","Late Feb","Early Mar","Late Mar","Early April","Late April","Early May","Late May")
-date_stamp <- c(1,2,3,4,5,6,7,8,9,10)
+date_stamp <- c(1:48)
 dd <- data.frame(date_stamp,mean_temp,num_confirmed,total_precip)
 dd <- subset(dd, !is.na(mean_temp))
 summary(dd)
 a<-79.333
 b<-406.6667
 p <- ggplot(dd, aes(x = date_stamp))+
-  geom_line(aes(y = mean_temp, color = "mean temperature"))+
-  geom_line(aes(y = (num_confirmed - b)/a, color = "num of confirmed cases"))+
+  geom_point(aes(y = mean_temp, color = "mean temperature"))+
+  geom_point(aes(y = (num_confirmed - b)/a, color = "num of confirmed cases"))+
   scale_y_continuous(sec.axis = sec_axis(~.*a + b, name = "num of confirmed cases"))+
   scale_color_manual(values = c("red","green"))+
   labs(title = "Halifax")+
@@ -117,8 +148,9 @@ mean_temp <- c()
 num_confirmed <-c()
 total_precip <- c()
 
-for (m in 1:5)
-{temp_set <- subset(Montreal_merge, Month == m) 
+for (y in 2020:2021)
+{for (m in 1:12)
+{temp_set <- subset(Montreal_merge, Month == m & Year == y) 
 print(m)
 early_month <- subset(temp_set, Day>=1 & Day<15)
 early_month <- subset(early_month, !is.na(Mean.Temp...C.))
@@ -130,17 +162,17 @@ num_confirmed[(length(num_confirmed) + 1)] <- sum(early_month[,16])
 num_confirmed[(length(num_confirmed) + 1)] <- sum(late_month[,16])
 total_precip[(length(total_precip) + 1)] <- sum(early_month[,64])
 total_precip[(length(total_precip) + 1)] <- sum(late_month[,64])
-}
+}}
 #date_stamp <- c("Early Jan","Late Jan","Early Feb","Late Feb","Early Mar","Late Mar","Early April","Late April","Early May","Late May")
-date_stamp <- c(1,2,3,4,5,6,7,8,9,10)
+date_stamp <- c(1:48)
 dd <- data.frame(date_stamp,mean_temp,num_confirmed,total_precip)
 dd <- subset(dd, !is.na(mean_temp))
 summary(dd)
 a<-60
 b<-600
 p <- ggplot(dd, aes(x = date_stamp))+
-  geom_line(aes(y = mean_temp, color = "mean temperature"))+
-  geom_line(aes(y = (num_confirmed - b)/a, color = "num of confirmed cases"))+
+  geom_point(aes(y = mean_temp, color = "mean temperature"))+
+  geom_point(aes(y = (num_confirmed - b)/a, color = "num of confirmed cases"))+
   scale_y_continuous(sec.axis = sec_axis(~.*a + b, name = "num of confirmed cases"))+
   scale_color_manual(values = c("red","green"))+
   labs(title = "Montreal")+
@@ -151,8 +183,8 @@ ggsave(p,filename = "pMontreal.png",width = 10,height = 10,limitsize = F)
 a<-13.333
 b<<-0
 p <- ggplot(dd, aes(x = date_stamp))+
-  geom_line(aes(y = total_precip, color = "total precip"))+
-  geom_line(aes(y = (num_confirmed - b)/a, color = "num of confirmed cases"))+
+  geom_point(aes(y = total_precip, color = "total precip"))+
+  geom_point(aes(y = (num_confirmed - b)/a, color = "num of confirmed cases"))+
   scale_y_continuous(sec.axis = sec_axis(~.*a + b, name = "num of confirmed cases"))+
   scale_color_manual(values = c("red","green"))+
   labs(title = "Montreal")+
@@ -171,8 +203,9 @@ StJohn_merge <- merge(StJohn_data, StJohn_climate,by = 'date', all=F)
 mean_temp <- c()
 num_confirmed <-c()
 total_precip <- c()
-for (m in 1:5)
-{temp_set <- subset(StJohn_merge, Month == m) 
+for (y in 2020:2021)
+{for (m in 1:12)
+{temp_set <- subset(StJohn_merge, Month == m & Year == y) 
 print(m)
 early_month <- subset(temp_set, Day>=1 & Day<15)
 early_month <- subset(early_month, !is.na(Mean.Temp...C.))
@@ -184,18 +217,18 @@ num_confirmed[(length(num_confirmed) + 1)] <- sum(early_month[,16])
 num_confirmed[(length(num_confirmed) + 1)] <- sum(late_month[,16])
 total_precip[(length(total_precip) + 1)] <- sum(early_month[,64])
 total_precip[(length(total_precip) + 1)] <- sum(late_month[,64])
-
+}
 }
 #date_stamp <- c("Early Jan","Late Jan","Early Feb","Late Feb","Early Mar","Late Mar","Early April","Late April","Early May","Late May")
-date_stamp <- c(1,2,3,4,5,6,7,8,9,10)
+date_stamp <- c(1:48)
 dd <- data.frame(date_stamp,mean_temp,num_confirmed,total_precip)
 dd <- subset(dd, !is.na(mean_temp))
 summary(dd)
 a<-26.36364
 b<-131.8182
 p <- ggplot(dd, aes(x = date_stamp))+
-  geom_line(aes(y = mean_temp, color = "mean temperature"))+
-  geom_line(aes(y = (num_confirmed - b)/a, color = "num of confirmed cases"))+
+  geom_point(aes(y = mean_temp, color = "mean temperature"))+
+  geom_point(aes(y = (num_confirmed - b)/a, color = "num of confirmed cases"))+
   scale_y_continuous(sec.axis = sec_axis(~.*a + b, name = "num of confirmed cases"))+
   scale_color_manual(values = c("red","green"))+
   labs(title = "StJohn")+
@@ -206,8 +239,8 @@ ggsave(p,filename = "pStJohn.png",width = 10,height = 10,limitsize = F)
 a<-1.611111
 b<-0
 p <- ggplot(dd, aes(x = date_stamp))+
-  geom_line(aes(y = total_precip, color = "total precip"))+
-  geom_line(aes(y = (num_confirmed - b)/a, color = "num of confirmed cases"))+
+  geom_point(aes(y = total_precip, color = "total precip"))+
+  geom_point(aes(y = (num_confirmed - b)/a, color = "num of confirmed cases"))+
   scale_y_continuous(sec.axis = sec_axis(~.*a + b, name = "num of confirmed cases"))+
   scale_color_manual(values = c("red","green"))+
   labs(title = "StJohn")+
@@ -231,8 +264,9 @@ Toronto_merge <- merge(Toronto_data, Toronto_climate,by = 'date', all=F)
 mean_temp <- c()
 num_confirmed <-c()
 total_precip <- c()
-for (m in 1:5)
-{temp_set <- subset(Toronto_merge, Month == m) 
+for (y in 2020:2021)
+{for (m in 1:12)
+{temp_set <- subset(Toronto_merge, Month == m & Year==y) 
 print(m)
 early_month <- subset(temp_set, Day>=1 & Day<15)
 early_month <- subset(early_month, !is.na(Mean.Temp...C.))
@@ -244,18 +278,18 @@ num_confirmed[(length(num_confirmed) + 1)] <- sum(early_month[,16])
 num_confirmed[(length(num_confirmed) + 1)] <- sum(late_month[,16])
 total_precip[(length(total_precip) + 1)] <- sum(early_month[,64])
 total_precip[(length(total_precip) + 1)] <- sum(late_month[,64])
-
+}
 }
 #date_stamp <- c("Early Jan","Late Jan","Early Feb","Late Feb","Early Mar","Late Mar","Early April","Late April","Early May","Late May")
-date_stamp <- c(1,2,3,4,5,6,7,8,9,10)
+date_stamp <- c(1:48)
 dd <- data.frame(date_stamp,mean_temp,num_confirmed,total_precip)
 dd <- subset(dd, !is.na(mean_temp))
 summary(dd)
 a<-3529.412
 b<34705.88
 p <- ggplot(dd, aes(x = date_stamp))+
-  geom_line(aes(y = mean_temp, color = "mean temperature"))+
-  geom_line(aes(y = (num_confirmed - b)/a, color = "num of confirmed cases"))+
+  geom_point(aes(y = mean_temp, color = "mean temperature"))+
+  geom_point(aes(y = (num_confirmed - b)/a, color = "num of confirmed cases"))+
   scale_y_continuous(sec.axis = sec_axis(~.*a + b, name = "num of confirmed cases"))+
   scale_color_manual(values = c("red","green"))+
   labs(title = "Toronto")+
@@ -267,8 +301,8 @@ ggsave(p,filename = "pToronto.png",width = 10,height = 10,limitsize = F)
 a<-1060
 b<12000
 p <- ggplot(dd, aes(x = date_stamp))+
-  geom_line(aes(y = total_precip, color = "total precip"))+
-  geom_line(aes(y = (num_confirmed - b)/a, color = "num of confirmed cases"))+
+  geom_point(aes(y = total_precip, color = "total precip"))+
+  geom_point(aes(y = (num_confirmed - b)/a, color = "num of confirmed cases"))+
   scale_y_continuous(sec.axis = sec_axis(~.*a + b, name = "num of confirmed cases"))+
   scale_color_manual(values = c("red","green"))+
   labs(title = "Toronto")+
@@ -288,8 +322,9 @@ Vancouver_merge <- merge(Vancouver_data, Vancouver_climate,by = 'date', all=F)
 mean_temp <- c()
 num_confirmed <-c()
 total_precip <- c()
-for (m in 1:5)
-{temp_set <- subset(Vancouver_merge, Month == m) 
+for (y in 2020:2021)
+{for (m in 1:12)
+{temp_set <- subset(Vancouver_merge, Month == m & Year == y) 
 print(m)
 early_month <- subset(temp_set, Day>=1 & Day<15)
 early_month <- subset(early_month, !is.na(Mean.Temp...C.))
@@ -301,18 +336,18 @@ num_confirmed[(length(num_confirmed) + 1)] <- sum(early_month[,16])
 num_confirmed[(length(num_confirmed) + 1)] <- sum(late_month[,16])
 total_precip[(length(total_precip) + 1)] <- sum(early_month[,64])
 total_precip[(length(total_precip) + 1)] <- sum(late_month[,64])
-
+}
 }
 #date_stamp <- c("Early Jan","Late Jan","Early Feb","Late Feb","Early Mar","Late Mar","Early April","Late April","Early May","Late May")
-date_stamp <- c(1,2,3,4,5,6,7,8,9,10)
+date_stamp <- c(1:48)
 dd <- data.frame(date_stamp,mean_temp,num_confirmed,total_precip)
 dd <- subset(dd, !is.na(mean_temp))
 summary(dd)
 a<-1000
 b<3000
 p <- ggplot(dd, aes(x = date_stamp))+
-  geom_line(aes(y = mean_temp, color = "mean temperature"))+
-  geom_line(aes(y = (num_confirmed - b)/a, color = "num of confirmed cases"))+
+  geom_point(aes(y = mean_temp, color = "mean temperature"))+
+  geom_point(aes(y = (num_confirmed - b)/a, color = "num of confirmed cases"))+
   scale_y_continuous(sec.axis = sec_axis(~.*a + b, name = "num of confirmed cases"))+
   scale_color_manual(values = c("red","green"))+
   labs(title = "Vancouver")+
@@ -323,8 +358,8 @@ ggsave(p,filename = "pVancouver.png",width = 10,height = 10,limitsize = F)
 a<-85.71429
 b<3000
 p <- ggplot(dd, aes(x = date_stamp))+
-  geom_line(aes(y = total_precip, color = "total precip"))+
-  geom_line(aes(y = (num_confirmed - b)/a, color = "num of confirmed cases"))+
+  geom_point(aes(y = total_precip, color = "total precip"))+
+  geom_point(aes(y = (num_confirmed - b)/a, color = "num of confirmed cases"))+
   scale_y_continuous(sec.axis = sec_axis(~.*a + b, name = "num of confirmed cases"))+
   scale_color_manual(values = c("red","green"))+
   labs(title = "Vancouver")+
